d9a6f1c6d6fa9ddbf874e775d2ecb54041b62aa0
main <main@DESKTOP-3MHE6N7>
2022-09-03 22:06:08 +0900

    lombok!설치

--- src/main/java/com/practise/jpa/entity/UserEntity.java
+++ src/main/java/com/practise/jpa/entity/UserEntity.java
@@ -15,7 +15,6 @@ import lombok.Data;
 @Table(name = "jpatest")
 @SequenceGenerator(sequenceName = "jpatestseq", allocationSize = 1, name = "jpaseq")
 public class UserEntity {
-
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "jpaseq")
@@ -27,35 +26,4 @@ public class UserEntity {
 	@Column(nullable = false)
 	private String password;
 
-	@Override
-	public String toString() {
-		return "UserEntity [id=" + id + ", username=" + username + ", password=" + password + "]";
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
-	
 }
